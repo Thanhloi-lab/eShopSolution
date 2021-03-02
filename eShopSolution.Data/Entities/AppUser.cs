7f9ad7ae3cced28c20a5b63b194fd8eef5d27cf5
caothanhloi2705@gmail.com <caothanhloi2705@gmail.com>
2021-03-02 21:01:40 +0700

    Identity user added

---
+++ eShopSolution.Data/Entities/AppUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Entities
+{
+    public class AppUser : IdentityUser<Guid>
+    {
+        string firstName;
+        string lastName;
+        DateTime dob;
+
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public DateTime Dob { get => dob; set => dob = value; }
+        public List<Cart> Carts { get; set; }
+        public List<Order> Orders { get; set; }
+        public List<Transaction> Transactions { get; set; }
+    }
+}
