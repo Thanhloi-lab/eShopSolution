feffb079ab0cf1ee2d0ca7014abd48134b7e053f
caothanhloi2705@gmail.com <caothanhloi2705@gmail.com>
2021-03-08 20:27:33 +0700

    Add Appuser Api

---
+++ eShopSolution.Application/System/Users/IUserService.cs
@@ -0,0 +1,23 @@
+﻿using eShopSolution.ViewModels.System.Users;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Application.System.Users
+{
+    public interface IUserService
+    {
+        Task<string> Authenticate(LoginRequest request);
+
+        Task<bool> Register(RegisterRequest request);
+
+        //Task<PageResult<UserViewModel>> GetUserPaging(GetUserPagingRequest request);
+        //Task<bool> Update(Guid id, UpdateRequest request);
+        //Task<bool> RoleAssign(Guid id, RoleAssignRequest request);
+
+        //Task<UserViewModel> GetById(Guid id);
+
+        //Task<bool> Delete(Guid Id);
+    }
+}
---
+++ eShopSolution.Application/System/Users/UserService.cs
@@ -0,0 +1,79 @@
+﻿using eShopSolution.Data.Entities;
+using eShopSolution.ViewModels.System.Users;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Application.System.Users
+{
+    public class UserService : IUserService
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly RoleManager<AppRole> _roleManager;
+        private readonly IConfiguration _configuration;
+        
+        public UserService(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, IConfiguration configuration,
+            RoleManager<AppRole> roleManager)
+        {
+            _configuration = configuration;
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task<string> Authenticate(LoginRequest request)
+        {
+            var user = await _userManager.FindByNameAsync(request.UserName);
+            if (user == null)
+                return null;
+            var result = await _signInManager.PasswordSignInAsync(user, request.Password, request.RememberMe, true);
+            if (!result.Succeeded)
+                return null;
+            var roles = _userManager.GetRolesAsync(user);
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.GivenName, user.FirstName),
+                new Claim(ClaimTypes.Role, string.Join(";", roles)),
+                new Claim(ClaimTypes.Name, user.LastName)
+            };
+
+            //the document: https://topdev.vn/blog/json-web-token-la-gi/
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Tokens:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_configuration["Tokens:Issuer"],
+              _configuration["Tokens:Issuer"],
+              claims,
+              expires: DateTime.Now.AddMinutes(180),
+              signingCredentials: credentials);
+            
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public async Task<bool> Register(RegisterRequest request)
+        {
+            var user = new AppUser()
+            {
+
+                Dob = request.Dob,
+                UserName = request.UserName,
+                Email = request.Email,
+                PhoneNumber = request.PhoneNumber,
+                FirstName = request.FirstName,
+                LastName = request.LastName
+            };
+            var result = await _userManager.CreateAsync(user, request.Password);
+            if (result.Succeeded)
+                return true;
+            return false;
+        }
+    }
+}
